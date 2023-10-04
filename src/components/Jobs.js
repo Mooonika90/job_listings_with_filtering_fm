e709c94453ba8e967e9e7bf0c34300e6588a1e8b
Mooonika90 <107703203+Mooonika90@users.noreply.github.com>
2023-10-04 21:09:20 +0200

    update

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,13 +1,46 @@
+import { useState } from 'react';
 import { Data } from '../data/data';
 
 function Jobs() {
+	const [selectedLanguages, setSelectedLanguages] = useState([]);
+	const [selectedLevel, setSelectedLevel] = useState('');
+	const [selectedRole, setSelectedRole] = useState('');
+
+	const filterJobs = (job) => {
+		const hasSelectedLanguages =
+			selectedLanguages.length === 0 ||
+			selectedLanguages.every((lang) => job.languages.includes(lang));
+		const hasSelectedLevel =
+			selectedLevel === '' || job.level === selectedLevel;
+		const hasSelectedRole = selectedRole === '' || job.role === selectedRole;
+		return hasSelectedLanguages && hasSelectedLevel && hasSelectedRole;
+	};
+	const filteredJobs = Data.filter(filterJobs);
+	const handleLanguageClick = (language) => {
+		// Check if the language is already selected
+		if (selectedLanguages.includes(language)) {
+			// If selected, remove it from the selectedLanguages state
+			setSelectedLanguages(
+				selectedLanguages.filter((lang) => lang !== language)
+			);
+		} else {
+			// If not selected, add it to the selectedLanguages state
+			setSelectedLanguages([...selectedLanguages, language]);
+		}
+	};
+
 	return (
 		<>
-			{Data.map((job) => (
+			{filteredJobs.map((job) => (
 				<article key={job.id}>
 					<div>
 						<div>
-							<h2>{job.position}</h2>
+							<div className='position-info'>
+								<h2>{job.company}</h2>
+								<span>{job.new}</span>
+								<span>{job.featured}</span>
+							</div>
+							<h3>{job.position}</h3>
 							<ul>
 								<li>{job.postedAt}</li>
 								<li>{job.contract}</li>
@@ -17,9 +50,18 @@ function Jobs() {
 						<img src={job.logo} alt={job.position} />
 					</div>
 
-					<ul className="lang-list">
-						{job.languages.map((lang, index) => (
-							<li key={index}>{lang}</li>
+					<ul className='lang-list'>
+						<li>{job.role}</li>
+						<li>{job.level}</li>
+						{job.languages.map((language, index) => (
+							<li
+								key={index}
+								className={
+									selectedLanguages.includes(language) ? 'selected' : ''
+								}
+								onClick={() => handleLanguageClick(language)}>
+								{language}
+							</li>
 						))}
 					</ul>
 				</article>
