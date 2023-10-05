1aeb292c1b00b0ecbec78e859eb9723b850f4b5a
Mooonika90 <107703203+Mooonika90@users.noreply.github.com>
2023-10-05 08:24:36 +0200

    update

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -6,67 +6,79 @@ function Jobs() {
 	const [selectedLevel, setSelectedLevel] = useState('');
 	const [selectedRole, setSelectedRole] = useState('');
 
-	const filterJobs = (job) => {
+	const filterJobs = ({ languages, level, role, ...job }) => {
 		const hasSelectedLanguages =
 			selectedLanguages.length === 0 ||
-			selectedLanguages.every((lang) => job.languages.includes(lang));
-		const hasSelectedLevel =
-			selectedLevel === '' || job.level === selectedLevel;
-		const hasSelectedRole = selectedRole === '' || job.role === selectedRole;
+			selectedLanguages.every((lang) => languages.includes(lang));
+		const hasSelectedLevel = selectedLevel === '' || level === selectedLevel;
+		const hasSelectedRole = selectedRole === '' || role === selectedRole;
 		return hasSelectedLanguages && hasSelectedLevel && hasSelectedRole;
 	};
-	const filteredJobs = Data.filter(filterJobs);
+
 	const handleLanguageClick = (language) => {
-		// Check if the language is already selected
 		if (selectedLanguages.includes(language)) {
-			// If selected, remove it from the selectedLanguages state
 			setSelectedLanguages(
 				selectedLanguages.filter((lang) => lang !== language)
 			);
 		} else {
-			// If not selected, add it to the selectedLanguages state
 			setSelectedLanguages([...selectedLanguages, language]);
 		}
 	};
 
 	return (
 		<>
-			{filteredJobs.map((job) => (
-				<article key={job.id}>
-					<div>
+			{Data.filter(filterJobs).map(
+				({
+					id,
+					company,
+					new: isNew,
+					featured,
+					position,
+					postedAt,
+					contract,
+					location,
+					logo,
+					role,
+					level,
+					languages,
+				}) => (
+					<article key={id} className={featured ? 'featured-border' : ''}>
 						<div>
-							<div className='position-info'>
-								<h2>{job.company}</h2>
-								<span>{job.new}</span>
-								<span>{job.featured}</span>
+							<div>
+								<div className='position-info'>
+									<h2>{company}</h2>
+									{isNew && <span className='position-new'>NEW!</span>}
+									{featured && <span className='featured'>featured</span>}
+								</div>
+								<h3>{position}</h3>
+								<ul>
+									<li>{postedAt}</li>
+									<li>{contract}</li>
+									<li>{location}</li>
+								</ul>
 							</div>
-							<h3>{job.position}</h3>
-							<ul>
-								<li>{job.postedAt}</li>
-								<li>{job.contract}</li>
-								<li>{job.location}</li>
-							</ul>
+							<img src={logo} alt={position} />
 						</div>
-						<img src={job.logo} alt={job.position} />
-					</div>
 
-					<ul className='lang-list'>
-						<li>{job.role}</li>
-						<li>{job.level}</li>
-						{job.languages.map((language, index) => (
-							<li
-								key={index}
-								className={
-									selectedLanguages.includes(language) ? 'selected' : ''
-								}
-								onClick={() => handleLanguageClick(language)}>
-								{language}
-							</li>
-						))}
-					</ul>
-				</article>
-			))}
+						<ul className='lang-list'>
+							<li>{role}</li>
+							<li>{level}</li>
+							{languages.map((language, index) => (
+								<li
+									key={index}
+									className={
+										selectedLanguages.includes(language) ? 'selected' : ''
+									}
+									onClick={() => handleLanguageClick(language)}>
+									{language}
+								</li>
+							))}
+						</ul>
+					</article>
+				)
+			)}
 		</>
 	);
 }
+
 export default Jobs;
